e48b606b84806deab341186653c5631099ce4cc0
Sato-Eisaku <ouc0149eisa@gmail.com>
2022-07-22 17:58:39 +0900

    #1 jsonから、アイコンやテキストのデータを持ってきて表示する

--- lib/main.dart
+++ lib/main.dart
@@ -45,24 +45,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   Color backgroundColor = Color.fromARGB(255, 5, 0, 13);
-  // json関係
-  late SampleModel _samples;
-  late Map cardData;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _jsonFileLoad().then((value) {
-      setState(() {
-        _samples = value;
-        print(_samples.textTraffic);
-      });
     });
   }
 
@@ -72,85 +58,105 @@ class _MyHomePageState extends State<MyHomePage> {
     DateTime _now = DateTime.now();
     DateFormat dateFormat = DateFormat('mm:ss');
     String timeString = dateFormat.format(_now);
-    return Scaffold(
-      // bodyの背景色を設定
-      backgroundColor: backgroundColor,
-      // appbarの実装
-      appBar: AppBar(
-        // 左側の戻るボタン
-        leading: IconButton(
-          padding: EdgeInsets.zero,
-          constraints: const BoxConstraints(
-            minHeight: 24.0,
-            minWidth: 24.0,
-          ),
-          iconSize: 28,
-          icon: Icon(Icons.arrow_back_ios_new),
-          onPressed: () {
-          },
-        ),
-        // タイトル
-        title: Text(widget.title),
-        // タイトルを左よせに
-        centerTitle: false,
-        // 右側のメニューボタン
-        actions: <Widget>[
-          IconButton(
-            iconSize: 28,
-            icon: Icon(Icons.menu),
-            onPressed: () {
-            },
-          ),
-        ],
-        // appbarの境界線を無くす
-        elevation: 0,
-        // appbarの背景色をbodyの背景色と同じに
-        backgroundColor: backgroundColor,
-        // appbarの高さを調整
-        toolbarHeight: size.height * 0.07,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            for (int i=0; i<4; i++)...{
-              TimeTableCard(
-                size: size,
-                now: _now,
+    print(_now);
+    return FutureBuilder(
+      future:getMap(),
+      builder:(context,snapshot){
+        if(snapshot.hasData){
+          Map<String,dynamic> _map = snapshot.data as Map<String,dynamic>;
+          return Scaffold(
+            // bodyの背景色を設定
+            backgroundColor: backgroundColor,
+            // appbarの実装
+            appBar: AppBar(
+              // 左側の戻るボタン
+              leading: IconButton(
+                padding: EdgeInsets.zero,
+                constraints: const BoxConstraints(
+                  minHeight: 24.0,
+                  minWidth: 24.0,
+                ),
+                iconSize: 28,
+                icon: Icon(Icons.arrow_back_ios_new),
+                onPressed: () {
+
+                },
+              ),
+              // タイトル
+              title: Text(widget.title),
+              // タイトルを左よせに
+              centerTitle: false,
+              // 右側のメニューボタン
+              actions: <Widget>[
+                IconButton(
+                  iconSize: 28,
+                  icon: Icon(Icons.menu),
+                  onPressed: () {
+                  },
+                ),
+              ],
+              // appbarの境界線を無くす
+              elevation: 0,
+              // appbarの背景色をbodyの背景色と同じに
+              backgroundColor: backgroundColor,
+              // appbarの高さを調整
+              toolbarHeight: size.height * 0.07,
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  for (String mapKey in _map.keys)...{
+                    TimeTableCard(
+                      map: _map,
+                      mapKey: mapKey,
+                      size: size,
+                      now: _now,
+                    )
+                  }
+                ]
               )
-            }
-          ]
-        )
-      ),
+            ),
+          );
+        }else{
+          return Scaffold(
+            body:Center(
+              child:Text("読み込み中です...")
+            )
+          );
+        }
+      }
     );
   }
 
   // timetable.jsonの読み込み
-  // 非同期処理らしい
-  Future<dynamic> _jsonFileLoad() async {
-    String path = 'json/timetable.json';
+  // 非同期処理
+  Future<Map<String, dynamic>> getMap() async {
+    String path = "json/timetable.json";
     String jsonString;
-    // 例外処理
-    try {
-      jsonString = await rootBundle.loadString(path);
-    } catch(e) {
-      print(e);
-      exit(0);
-    }
-
-    return SampleModel.fromJson(jsonDecode(jsonString));
+    jsonString = await rootBundle.loadString(path);
+    Map<String,dynamic> result = jsonDecode(jsonString);
+    return result;
   }
 }
 
 class TimeTableCard extends StatelessWidget{
-  TimeTableCard({Key? key, required this.now, required this.size}) : super(key: key);
+  TimeTableCard({Key? key, required this.map, required this.mapKey, required this.now, required this.size}) : super(key: key);
+  Map<String, dynamic> map;
+  String mapKey;
   Size size;
   DateTime now;
   DateFormat dateFormat = DateFormat('HH:mm');
+  Map<String, IconData> icons = {"bus": MyIcons.bus, "train": MyIcons.train, "graduation_hat": MyIcons.graduation_hat, "apartment": MyIcons.apartment};
 
   @override
   Widget build(BuildContext context){
     String dateString = dateFormat.format(now);
+    IconData? iconTrafic = icons[map[mapKey]["iconTraffic"]];
+    String textTraffic = map[mapKey]["textTraffic"];
+    IconData? iconDestination = icons[map[mapKey]["iconDestination"]];
+    String textDestination = map[mapKey]["textDestination"];
+    print(textTraffic);
     // ジェスチャー（タップ、ドラッグ等）を可能にするため、CardをGestureDetectorで囲む
     return GestureDetector(
       behavior: HitTestBehavior.deferToChild,
@@ -165,7 +171,7 @@ class TimeTableCard extends StatelessWidget{
           color: Color.fromARGB(255, 37, 37, 40),
           elevation: 3,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 width: size.width * 0.4,
@@ -174,47 +180,75 @@ class TimeTableCard extends StatelessWidget{
                   children: [
                     // 交通機関を示すアイコン
                     Container(
-                      child: Column(
+                      padding: EdgeInsets.all(4),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
+                          // 交通機関情報
+                          Column(
+                            children: [
+                              // 交通機関のアイコン
+                              Icon(
+                                iconTrafic,
+                                color: Colors.white,
+                                size: size.width*0.12,
+                              ),
+                              SizedBox(
+                                height: 6,
+                              ),
+                              // 交通機関のテキスト
+                              Text(
+                                textTraffic,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: size.width * 0.04,
+                                ),
+                              ),
+                            ],
+                          ),
+                          // 右矢印アイコン
                           Text(
-                            "JR",
+                            "to",
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: size.width * 0.04,
-                            ),
+                              fontSize: size.width * 0.06
+                            )
                           ),
-                          Icon(
-                            MyIcons.train,
+                          /* Icon(
+                            MyIcons.arrow_right,
                             color: Colors.white,
-                            size: size.width*0.12,
+                            size: size.width*0.06,
+                          ), */
+                          // 目的地情報
+                          Column(
+                            children: [
+                              // 目的地のアイコン
+                              Icon(
+                                iconDestination,
+                                color: Colors.white,
+                                size: size.width*0.12,
+                              ),
+                              SizedBox(
+                                height: 6,
+                              ),
+                              // 目的地のテキスト
+                              Text(
+                                textDestination,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: size.width * 0.04,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ),
-                    // 出発地、到着地を示すアイコン
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(
-                          MyIcons.apartment,
-                          color: Colors.white,
-                          size: size.width*0.09,
-                        ),
-                        Icon(
-                          MyIcons.arrow_right,
-                          color: Colors.white,
-                          size: size.width*0.06,
-                        ),
-                        Icon(
-                          MyIcons.graduation_hat,
-                          color: Colors.white,
-                          size: size.width*0.09,
-                        ),
-                      ],
-                    )
                   ]
                 ),
               ),
+              // 時刻等の表示
               Container(
                 width: size.width * 0.4,
                 child: Column(
